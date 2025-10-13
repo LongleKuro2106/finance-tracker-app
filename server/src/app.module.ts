47fa20fc1e475f589c3611844fa395d019f07694
Long Le <long.le21062004@gmail.com>
2025-10-13 12:36:09 +0300

    Adjust the schema of prisma no longer use Account balance, add in transaction endpoint for user using CRUD operation, update and optimize dto

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/users/dto/update-user.dto.ts
+++ server/src/users/dto/update-user.dto.ts
@@ -1,4 +1,23 @@
-import { CreateUserDto } from './create-user.dto';
-import { PartialType } from '@nestjs/mapped-types';
+import { IsEmail, IsEnum, IsOptional, IsString, MaxLength } from 'class-validator';
+import { Role } from '@prisma/client';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  password?: string;
+
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(100)
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
+}
